d8e5a239b6626ca87787d6e33ec0d4042f2ef386
Aidan747 <aidan747-8@Outlook.com>
2023-05-05 15:04:00 -0500

    backend boilerplate start

---
+++ API/RestfulAPI.h
@@ -0,0 +1,8 @@
+#ifndef RestfulAPI_H
+#define RestfulAPI_H
+
+class RestfulAPI {
+    
+};
+
+#endif
---
+++ API/TBA.h
@@ -0,0 +1,11 @@
+#ifndef TBA_H
+#define TBA_H
+
+#include "RestfulAPI.h"
+
+class TBA : public RestfulAPI {
+    
+};
+
+
+#endif
