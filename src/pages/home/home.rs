4f957cc76dd50b178d465a322bc4e7fa0997364f
Aidan747 <93739159+Aidan747@users.noreply.github.com>
2024-01-23 00:05:38 -0600

    css changes

--- src/pages/home/home.rs
+++ src/pages/home/home.rs
@@ -15,43 +15,59 @@ use tba::*;
 #[component]
 pub fn HomePage(cx: Scope) -> impl IntoView {
     
-    let matches = get_matches("4645".to_string(),"2023ilch".to_string()).await.unwrap_or(vec![
-            Match {
-                actual_time: None,
-                alliances: None,
-                comp_level: None,
-                event_key: None,
-                key: None,
-                match_number: None,
-                post_result_time: None,
-                predicted_time: None,
-                score_breakdown: None,
-                set_number: None,
-                time: None,
-                videos: None,
-                winning_alliance: None 
-            }
-        ]
-    );
+    let match_arr = create_resource(cx, || (), |_| async move {
+        get_matches("4645".to_string(), "2023ilch".to_string()).await;
+    });
 
-
+    
     view! { cx,
         <div class="flex flex-row bg-gray-100 h-screen">
             <Navbar/>
-            <div class="flex flex-row p-4 flex-grow">
-                <div class="p-6 flex-grow">
-                    <TeamCard team_number="4645".to_string()/>
+            <div class="flex flex-col p-4 flex-grow">
+                <div class="flex flex-row p-4 flex-grow">
+                    <div class="p-6 flex-grow">
+                        <TeamCard team_number="4645".to_string()/>
+                    </div>
+                    <div class="p-6 flex-grow">
+                        <RankCard team="4645".to_string()/>
+                    </div>
+                    <div class="p-6 flex-grow">
+                        <MatchCard event_code="4645".to_string()/>
+                    </div>                
                 </div>
-                <div class="p-6 flex-grow">
-                    <RankCard team="4645".to_string()/>
+                <div class="bg-white rounded-lg  flex flex-col p-4 flex-grow">
+                    <h1 class="text-2xl text-center"><b>"Upcoming Matches"</b></h1>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th class="w-1/2">"Red Blue Alliance Numbers"</th>
+                                <th class="w-1/2">"Blue Alliance Numbers"</th>
+                            </tr>
+                        </thead>
+                        <tbody class="h-40 overflow-y-scroll">
+                            <tr>
+                                <td class="bg-red-600">"---"</td>
+                                <td class="bg-blue-600">"---"</td>
+                            </tr>
+                            <tr>
+                                <td class="bg-red-600">"---"</td>
+                                <td class="bg-blue-600">"---"</td>
+                            </tr>
+                            <tr>
+                                <td class="bg-red-600">"---"</td>
+                                <td class="bg-blue-600">"---"</td>
+                            </tr>
+                            <tr>
+                                <td class="bg-red-600">"---"</td>
+                                <td class="bg-blue-600">"---"</td>
+                            </tr>
+                            <tr>
+                                <td class="bg-red-600">"---"</td>
+                                <td class="bg-blue-600">"---"</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
-                <div class="p-6 flex-grow">
-                    <MatchCard event_code="4645".to_string()/>
-                </div>
-            </div>
-            <div>
-                <table>
-                </table>
             </div>
         </div>
     }
--- src/pages/my_team/my_team.rs
+++ src/pages/my_team/my_team.rs
@@ -7,8 +7,37 @@ pub fn MyTeamPage(cx: Scope) -> impl IntoView {
 
     view! { cx,
         <div class="flex flex-row bg-gray-100 h-screen">
-            <Navbar/>
-            
+            <div class="w-[288px]">
+                <Navbar/>
+            </div>
+            <div class="flex flex-col flex-grow p-4">
+                <div class="flex flex-row flex-grow p-4">
+                    <div class="p-4">
+                        <div class="rounded-lg shadow-lg bg-white p-4 flex-grow h-64">
+                            <h1 class="text-6xl text-blue-900"><b>"Current Rank: -1"</b></h1>
+                            <br/>
+                            <h1 class="text-lg"><b>"Change of {None} from previous match"</b></h1>
+                        </div>
+                    </div>
+                    <div class="p-4">
+                        <div class="rounded-lg shadow-lg bg-white p-4 flex-grow h-64">
+                            <h1 class="text-6xl text-blue-900"><b>"Average Match Score: -1"</b></h1>
+                            <br/>
+                            <h1 class="text-lg"><b>"Change of {None} from previous match"</b></h1>
+                        </div>
+                    </div>
+                    <div class="p-4">
+                        <div class="rounded-lg shadow-lg bg-white p-4 flex-grow h-64">
+                            <h1 class="text-6xl text-blue-900"><b>"Next Match: -1"</b></h1>
+                            <br/>
+                            <h1 class="text-lg"><b>"{None} matches from now (in {None} minutes)"</b></h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-white rounded-lg h-96 text-center shadow-xl">
+                    <h1><b>"{Graph not loaded because no data is present}"</b></h1>
+                </div>
+            </div>
         </div>
     }
 }
